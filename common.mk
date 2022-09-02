850135bb21c4f0a31d9773ea84a8ff217c327eed
Arnaud EBALARD <arnaud.ebalard@ssi.gouv.fr>
2022-09-02 16:16:44 +0200

    Add PEDANTIC knob support in makefile for additional checks

--- common.mk
+++ common.mk
@@ -3,7 +3,9 @@ FPIE_CFLAG=-fPIE
 FPIE_LDFLAGS=-pie -Wl,-z,relro,-z,now
 STACK_PROT_FLAG=-fstack-protector-strong
 
-ifeq ($(CC),clang)
+CLANG :=  $(shell $(CROSS_COMPILE)$(CC) -v 2>&1 | grep clang)
+
+ifneq ($(CLANG),)
 # When compiler is *explicitly* set to clang, use its -Weverything option by
 # default but disable the sepcific options we cannot support:
 #
@@ -15,8 +17,41 @@ WARNING_CFLAGS = -Weverything -Werror \
 		 -Wno-unreachable-code-break \
 		 -Wno-covered-switch-default \
 		 -Wno-padded
-else
+
+# Clang version >= 13? Adapt
+CLANG_VERSION_GTE_13 := $(shell echo `$(CROSS_COMPILE)$(CC) -dumpversion | cut -f1-2 -d.` \>= 13.0 | sed -e 's/\./*100+/g' | bc)
+  ifeq ($(CLANG_VERSION_GTE_13), 1)
+  # We have to do this because the '_' prefix seems now reserved to builtins
+  CFLAGS += -Wno-reserved-identifier
+  endif
+
+ifeq ($(PEDANTIC),1)
+CFLAGS += -Werror -Walloca -Wcast-qual -Wconversion -Wformat=2           \
+	  -Wformat-security -Wnull-dereference -Wstack-protector -Wvla   \
+	  -Warray-bounds -Warray-bounds-pointer-arithmetic -Wassign-enum \
+	  -Wbad-function-cast -Wconditional-uninitialized -Wconversion   \
+	  -Wfloat-equal -Wformat-type-confusion -Widiomatic-parentheses  \
+	  -Wimplicit-fallthrough -Wloop-analysis -Wpointer-arith         \
+	  -Wshift-sign-overflow -Wshorten-64-to-32                       \
+	  -Wtautological-constant-in-range-compare                       \
+	  -Wunreachable-code-aggressive -Wthread-safety                  \
+	  -Wthread-safety-beta -Wcomma
+endif
+
+else # gcc
+
 WARNING_CFLAGS = -W -Werror -Wextra -Wall -Wunreachable-code
+ifeq ($(PEDANTIC),1)
+CFLAGS += -Wpedantic -Wformat=2 -Wformat-overflow=2 -Wformat-truncation=2      \
+	  -Wformat-security -Wnull-dereference -Wstack-protector -Wtrampolines \
+	  -Walloca -Wvla -Warray-bounds=2 -Wimplicit-fallthrough=3             \
+	  -Wshift-overflow=2 -Wcast-qual -Wstringop-overflow=4 -Wconversion    \
+	  -Wlogical-op -Wduplicated-cond                                       \
+	  -Wduplicated-branches -Wformat-signedness -Wshadow                   \
+	  -Wstrict-overflow=2 -Wundef -Wstrict-prototypes -Wswitch-default     \
+	  -Wcast-align=strict -Wjump-misses-init
+endif
+
 endif
 
 # If the user has overridden the CFLAGS or LDFLAGS, let's detect it
